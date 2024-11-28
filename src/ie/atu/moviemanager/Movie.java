5ef1ec1e57aed021579d6e5042c1738798260eae
Andrea Porter <145453726+G00436912@users.noreply.github.com>
2024-11-28 10:53:08 +0000

    Movie variables created

---
+++ src/ie/atu/moviemanager/Movie.java
@@ -0,0 +1,12 @@
+package ie.atu.moviemanager;
+
+public class Movie {
+
+    //instance variables
+    private String name; //name of movie
+    private double lengthMin; //minutes of movie
+    private float rating; //rating of reviews for movie 
+    private int age; //age for movie
+
+
+}
