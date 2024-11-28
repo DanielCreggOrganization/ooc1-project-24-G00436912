109208cdfe11ae91e50035cc3343ffd3155ff40d
Andrea Porter <145453726+G00436912@users.noreply.github.com>
2024-11-28 15:13:21 +0000

    creating objects

--- src/ie/atu/moviemanager/Main.java
+++ src/ie/atu/moviemanager/Main.java
@@ -5,7 +5,11 @@ public class Main {
     public static void main(String[] args) {
         
         //Create Object
-        Movie movieObject1 = new Movie()
+    Movie movieObject1 = new movie(Wicked,8.2,160,12)
+        System.out.println(movieObject1.getname());
+        System.out.println(movieObject1.getlengthMin());
+        System.out.println(movieObject1.getrating());
+        System.out.println(movieObject1.getage());
     }
 
 }
