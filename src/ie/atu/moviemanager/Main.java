5405ddec0d144c1111e2e05ae9572009810410b1
Andrea Porter <145453726+G00436912@users.noreply.github.com>
2024-11-28 16:02:00 +0000

    Display Menu

--- src/ie/atu/moviemanager/Main.java
+++ src/ie/atu/moviemanager/Main.java
@@ -17,5 +17,18 @@ public class Main {
     Scanner userInput = new Scanner(System.in);
     int userSelection = 0; //Used to store user Menu option selection
 
+    //Create infinite while loop to display Menu after every command
+    while(true){
+        //Display Menu to console
+        System.out.println("");
+        System.out.println("###############################");
+        System.out.println("# Studient Applicarion v0.9 #");
+        System.out.println("###############################");
+        System.out.println("(1) Add a Movie.");
+        System.out.println("(2) Delete a Movie.");
+        System.out.println("(3) Show total number of Movies.");
+        System.out.println("(4) Search for a Movie by Movie rating.");
+        System.out.println("(5) Quit.");
+        System.out.println("Select an option from 1 to 5 and press Enter");
 
 }
