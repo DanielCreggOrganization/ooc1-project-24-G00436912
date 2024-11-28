f1a54b4438fa3313c384db958bb20d3db44ce51f
Andrea Porter <145453726+G00436912@users.noreply.github.com>
2024-11-28 10:42:51 +0000

    started main

--- src/ie/atu/moviemanager/Main.java
+++ src/ie/atu/moviemanager/Main.java
@@ -2,4 +2,10 @@ package ie.atu.moviemanager;
 
 public class Main {
 
+    public static void main(String[] args) {
+        
+        //Create Object
+        Movie movieObject1 = new Movie()
+    }
+
 }
