4abe7a44ff66ca2c5527fe41f2d47f181390c9b4
Andrea Porter <145453726+G00436912@users.noreply.github.com>
2024-12-02 23:01:45 +0000

    Updated Genre new genre

--- src/ie/atu/moviemanager/Genre.java
+++ src/ie/atu/moviemanager/Genre.java
@@ -2,9 +2,15 @@ package ie.atu.moviemanager;
 //Inheritance,
 
 public class Genre extends Movie{
-    public Genre(String name, double lengthMin, float rating, int age){
+    private String Genre;
+    public Genre(String name, double lengthMin, float rating, int age, String Genre){
         super( name,lengthMin,rating,age);
+        this.Genre = Genre;
     }
+    public String getGenre() {
+        return this.Genre;
+    }
+
 
 
 }
