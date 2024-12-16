fb6945cbc5863425d9dc943a58b7d419d444a8e8
Andrea Porter <145453726+G00436912@users.noreply.github.com>
2024-12-16 16:43:38 +0000

    Added more comments

--- src/ie/atu/moviemanager/Genre.java
+++ src/ie/atu/moviemanager/Genre.java
@@ -1,7 +1,8 @@
 package ie.atu.moviemanager;
-//Inheritance,
+//Inheritance,uses extend to take the getters from movie class.
 
 public class Genre extends Movie{
+    //adds a genre field to movie
     private String Genre;
     public Genre(String name, double lengthMin, float rating, int age, String Genre){
         super( name,lengthMin,rating,age);
@@ -10,7 +11,7 @@ public class Genre extends Movie{
     public String getGenre() {
         return this.Genre;
     }
-    
+    //exactly like the overide in movie but just adds genre to it
     @Override 
     public String toString(){
         return "GenreMovie ="+this.getName()+" "+this.getLengthMin()+" "+this.getRating()+" "+this.getAge()+" "+this.getGenre()+".";
